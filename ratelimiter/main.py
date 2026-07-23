40d08bbe8039b9184905a4b9cb00fee7bc46459b
umang <umangpawar2005@gmail.com>
2026-07-23 23:58:20 +0530

    FIXED WINDOW COUNTER RATE LIMITER IMPLEMENTED

--- ratelimiter/main.py
+++ ratelimiter/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+import time
 
 app = FastAPI()
 
+request_limit=100
+time_limit = 60
 
-client_id = ["abcd1234","efgh5678"]
+
+client_id = {}
 
 
 @app.get("/")
@@ -12,8 +16,50 @@ def hello():
 
 @app.post("/check")
 def client_id_authenticate(client:str):
+    
+    
+   
+    
+
     if(client not in client_id):
-        client_id.append(client)
-        return {"message":"id authenticated"}
+        count = 1
+        windowStart = time.time()
+        client_id[client]={
+              "count":count,
+              "window":windowStart
+        }
+       
+      
+       
+
+        
+        return {"message":"id authenticated and request allowed",
+                "current_count":1}
     else:
-        return {"message":"your id exist already"}
+        currentTime = time.time()
+
+        elasped = currentTime - client_id[client]["window"]
+
+        if(elasped>=time_limit):
+                client_id[client]["window"] = time.time()
+                client_id[client]["count"] = 1
+        elif(client_id[client]["count"]<request_limit):
+                 client_id[client]["count"] += 1
+        else:
+                return{"message":"429 too many requests"}
+        
+             
+        
+                
+                
+                
+        
+        
+                
+        return {"message":"request allowed",
+                "current_count":client_id[client]["count"]}
+
+
+
+    
+
